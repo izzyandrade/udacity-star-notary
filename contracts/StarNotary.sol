75ac0a4ccc2d7cd9cea4303fb444f7d47c2845f2
izzyandrade <israelcarneiro97@gmail.com>
2022-09-21 22:55:26 -0300

    feat: created truffle environment with StarNotary smart contract

---
+++ contracts/StarNotary.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.8.7;
+
+contract StarNotary {
+    string public starName;
+    address public starOwner;
+
+    event starClaimed(address owner);
+
+    constructor() {
+        starName = "Izzy's Star";
+    }
+
+    function claimStar() public {
+        starOwner = msg.sender;
+        emit starClaimed(msg.sender);
+    }
+}
---
+++ test/StarNotary.js
@@ -0,0 +1,19 @@
+// Importing the StartNotary Smart Contract ABI (JSON representation of the Smart Contract)
+const StarNotary = artifacts.require('StarNotary');
+
+var accounts; // List of development accounts provided by Truffle
+var owner; // Global variable use it in the tests cases
+
+// This called the StartNotary Smart contract and initialize it
+contract('StarNotary', (accs) => {
+  accounts = accs; // Assigning test accounts
+  owner = accounts[0]; // Assigning the owner test account
+});
+
+// Example test case, it will test if the contract is able to return the starName property
+// initialized in the contract constructor
+it('has correct name', async () => {
+  let instance = await StarNotary.deployed(); // Making sure the Smart Contract is deployed and getting the instance.
+  let starName = await instance.starName.call(); // Calling the starName property
+  assert.equal(starName, "Izzy's Star"); // Assert if the starName property was initialized correctly
+});
